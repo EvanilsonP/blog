a925e30d3e6d2d54789fbd8d23284c36e4210567
EvanilsonP <pevanilson99@gmail.com>
2023-02-23 14:36:36 -0300

    rendering html content with ejs view engine

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.send('Hello!');
+    res.render('index');
 });
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create');
+});
+
+app.use((req, res) => {
+    res.status(404).render('404')
+})
 
 app.listen(3000);
