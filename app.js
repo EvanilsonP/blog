8e535d058cb34c6552a6a3e201c91c4dbe3c146e
EvanilsonP <pevanilson99@gmail.com>
2023-02-23 14:48:49 -0300

    using partials

--- app.js
+++ app.js
@@ -4,19 +4,24 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const blogs = [
+        { title: 'Mario', snippet: 'A good day', body: 'lopse' },
+        { title: 'Mario', snippet: 'A good day', body: 'lopse' },
+        { title: 'Mario', snippet: 'A good day', body: 'lopse' },
+    ]
+    res.render('index', { title: 'Home', blogs: blogs});
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', { title: 'About'});
 });
 
 app.get('/blogs/create', (req, res) => {
-    res.render('create');
+    res.render('create', { title: 'Create'});
 });
 
 app.use((req, res) => {
-    res.status(404).render('404')
+    res.status(404).render('404', { title: '404'})
 })
 
 app.listen(3000);
