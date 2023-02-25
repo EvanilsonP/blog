71853517e7688b96255687fc8ce213afa1113103
EvanilsonP <pevanilson99@gmail.com>
2023-02-25 10:07:49 -0300

    adding middleware

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const { infoMiddleware } = require('./middleware/infoMdlw');
 const app = express();
 
 app.set('view engine', 'ejs');
+
+app.use(infoMiddleware);
 
 app.get('/', (req, res) => {
     const blogs = [
---
+++ middleware/infoMdlw.js
@@ -0,0 +1,7 @@
+exports.infoMiddleware = (req, res, next) => {
+    console.log(`new request made: `);
+    console.log(`host: ${req.hostname}`);
+    console.log(`path: ${req.path}`);
+    console.log(`method: ${req.method}`);
+    next();
+}
